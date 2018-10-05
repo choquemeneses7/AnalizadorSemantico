5f1f3c33e543f0aa9c0ab71ac7b2eb531a6068a4
Nicolas Carrasco <chincholax@gmail.com>
2018-10-05 15:05:05 -0400

    master: arreglo entity, view, control

---
+++ src/control/Parser.java
@@ -0,0 +1,59 @@
+package control;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Queue;
+import java.util.Stack;
+
+import entity.Reglas;
+import entity.TerminalesYNoTerminales;
+
+
+public class Parser {
+
+	public static void parser(Queue<String> input, Stack<String> stack,
+			Map<TerminalesYNoTerminales, List<Reglas>> tabla, List<Reglas> producciones) {
+		//ESTA ES LA CLASE PRINCIPAL DONDE SE HACE TODA LA MAGIA BASICAMENTE VAMOS ANALIZANDO LAS REGLAS Y EN CASO DE 
+		//QUE LA CADENA COINCIDA CON LA REGLA  LA REEMPLAZAN Y LA APILAN EN UNA PILA
+		String fromEntrada = input.peek();
+		String fromPila = stack.peek();
+		ControlReglasYTerminales helper = new ControlReglasYTerminales(producciones);
+
+		while (!fromPila.equals("$")) {
+			System.out.print("\nStack:" + stack + "\tInput:" + input);
+
+			TerminalesYNoTerminales key = new TerminalesYNoTerminales(fromPila, fromEntrada);
+			if (fromEntrada.equals(fromPila)) {
+				fromPila = stack.pop();
+				fromEntrada = input.poll();
+				fromEntrada = input.peek();
+			}
+
+			else if (helper.esTerminal(fromPila)) {
+				System.out.println("Error al procesar la entrada.");
+				return;
+			} else if (tabla.get(key) == null) {
+				System.out.println("Error al procesar la entrada.");
+				return;
+			} else {
+				if (tabla.get(key).get(0).getDerecha().equals("sinc")) {
+					fromPila = stack.pop();
+
+				} else {
+					List<String> prod = tabla.get(key).get(0).getDerecha();
+					System.out.print("\tAccion:" + tabla.get(key).get(0));
+
+					fromPila = stack.pop();
+
+					for (int j = prod.size() - 1; j >= 0; j--) {
+						if (!prod.get(j).equals("vacio"))
+							stack.push(prod.get(j));
+					}
+				}
+
+			}
+			fromPila = stack.peek();
+		}
+	}
+
+}
---
+++ src/entity/TerminalesYNoTerminales.java
@@ -0,0 +1,52 @@
+package entity;
+
+public class TerminalesYNoTerminales {
+
+	private String terminal;
+	private String noTerminal;
+
+	public TerminalesYNoTerminales(String noTerminal, String terminal) {
+		this.noTerminal = noTerminal;
+		this.terminal = terminal;
+
+	}
+
+	@Override
+	public int hashCode() {
+		int hashcode = 1;
+		hashcode = 31 + terminal.hashCode();
+		hashcode = hashcode * 31 + noTerminal.hashCode();
+		return hashcode;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		TerminalesYNoTerminales other = (TerminalesYNoTerminales) obj;
+		if (!terminal.equals(other.terminal))
+			return false;
+		if (!noTerminal.equals(other.noTerminal))
+			return false;
+		return true;
+
+	}
+
+	@Override
+	public String toString() {
+		return "M[" + this.noTerminal + "," + this.terminal + "]";
+	}
+
+	public String getTerminal() {
+		return terminal;
+	}
+
+	public void setTerminal(String terminal) {
+		this.terminal = terminal;
+	}
+
+}
